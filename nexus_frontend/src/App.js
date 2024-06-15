1f93e3fa135c1e67e0b25cf484664530037ecbf4
RoySamson-stack <you@example.com>
2024-06-15 23:39:03 +0300

    making the data from the django backend populate the react frontend

--- nexus_frontend/src/App.js
+++ nexus_frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state  = {details : [],}
+
+    componentDidMount(){
+      
+
+      let data;
+      axios.get('https://localhost:8000')
+      .then(res =>{
+        data = res.data;
+        this.setState({
+          details: data
+        })
+      })
+      .catch(err => {
+
+      })
+    }
+    render(){
+      return(
+        <div>
+          <header>
+          Data rom django
+        </header>
+        <hr></hr>
+        {this.state.details.map((output, id)=>(
+          <div key={id}>
+            <div>
+              <h2>{output.employee}</h2>
+              <h2>{output.department}</h2>
+            </div>
+          </div>
+        ))}
+        </div>
+      )
+        
+        
+    }
 }
-
-export default App;
+export default App
