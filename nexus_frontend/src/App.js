db31847042a70d91ac07316983ad1173a0bb436a
RoySamson-stack <you@example.com>
2024-06-15 23:44:46 +0300

    fixing endpoint error

--- nexus_frontend/src/App.js
+++ nexus_frontend/src/App.js
@@ -1,43 +1,39 @@
-import axios from 'axios'
-import React from 'react'
+import axios from 'axios';
+import React from 'react';
 
 class App extends React.Component {
-    state  = {details : [],}
+  state = { details: [] };
 
-    componentDidMount(){
-      
-
-      let data;
-      axios.get('https://localhost:8000')
-      .then(res =>{
-        data = res.data;
-        this.setState({
-          details: data
-        })
+  componentDidMount() {
+    axios.get('http://localhost:8000/')
+      .then(res => {
+        this.setState({ details: res.data });
       })
       .catch(err => {
+        console.error("Error fetching data from Django:", err);
+      });
+  }
 
-      })
-    }
-    render(){
-      return(
-        <div>
-          <header>
-          Data rom django
-        </header>
-        <hr></hr>
-        {this.state.details.map((output, id)=>(
-          <div key={id}>
-            <div>
-              <h2>{output.employee}</h2>
-              <h2>{output.department}</h2>
+  render() {
+    return (
+      <div>
+        <header>Data from Django</header>
+        <hr />
+        {this.state.details.length === 0 ? (
+          <div>No data available</div>
+        ) : (
+          this.state.details.map((output, id) => (
+            <div key={id}>
+              <div>
+                <h2>{output.employee}</h2>
+                <h2>{output.department}</h2>
+              </div>
             </div>
-          </div>
-        ))}
-        </div>
-      )
-        
-        
-    }
+          ))
+        )}
+      </div>
+    );
+  }
 }
-export default App
+
+export default App;
