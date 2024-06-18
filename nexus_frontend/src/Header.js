812e57b3561bb502b76962517e7d5cfd6cf9deef
RoySamson-stack <you@example.com>
2024-06-18 16:04:55 +0300

    update of the front end

--- nexus_frontend/src/Header.js
+++ nexus_frontend/src/Header.js
@@ -4,15 +4,28 @@ import './App.css'
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/bugs">Bug Reports</a></li>
-          <li><a href="/submit">Submit Bug</a></li>
-        </ul>
+    // <header>
+    //   <nav>
+    //     <ul>
+    //       <li><a href="/">Home</a></li>
+    //       <li><a href="/bugs">Bug Reports</a></li>
+    //       <li><a href="/submit">Submit Bug</a></li>
+    //     </ul>
+    //   </nav>
+    // </header>
+    <div className="navbar">
+      <div>
+      </div>
+      <nav className='home-navbar'>
+        <a href="/">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="/bug-bounty">Bug Reports</a>
+        {/* <a href="submit">Submit</a> */}
+        {/* <a href="#">Pricing</a> */}
       </nav>
-    </header>
+      <button className="navbar-contactbtn">Contact us</button>
+    </div>
   );
 }
 
--- nexus_frontend/src/HomePage.js
+++ nexus_frontend/src/HomePage.js
@@ -1,14 +1,24 @@
-// Homepage.js
-
 import React from 'react';
 
 const Homepage = () => {
-  return (
-    <div className="homepage">
-      <h1>Welcome to Bug Bounty Dashboard!</h1>
-      <p>This is the homepage content.</p>
-    </div>
-  );
+    return (
+        <div className="home">
+            <div className="homepage">
+                <div className="homepage-2">
+                    <header className='home-header'>
+                        <h1>Cyber resilience,<br></br> redefined</h1>
+                        <p>Harness the power of ethical hackers and cutting-edge technology to fortify your
+                            defenses against evolving threats.Empower your security stratergy with unparalleled expertise and proactive protection, ensuring your digital assets stay securre in an unrepdictable landscape
+                        </p>
+                        <button className='home-btn'>Get started</button>
+                    </header>
+                </div>
+            </div>
+            <div className="">
+                
+            </div>
+        </div>
+    );
 }
 
 export default Homepage;
