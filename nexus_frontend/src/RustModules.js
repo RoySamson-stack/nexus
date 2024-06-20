9a8ee604d7c86cb71581ee1513f98d3d2583a958
RoySamson-stack <you@example.com>
2024-06-20 23:43:28 +0300

    addin the rust modukes for allerting the server intrusion to the system

--- nexus_frontend/src/RustModules.js
+++ nexus_frontend/src/RustModules.js
@@ -3,20 +3,37 @@ import './App.css';
 
 const RustModules = () => {
   const [modules, setModules] = useState([]);
+  const [serverInfo, setServerInfo] = useState('');
+  const [cloudInstances, setCloudInstances] = useState([]);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [verificationResult, setVerificationResult] = useState('');
 
   useEffect(() => {
     fetch('http://127.0.0.1:8082/modules')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => setModules(data))
       .catch(error => console.error('Error fetching modules:', error));
-  }, []);
 
-  console.log(modules); // Log modules to check in the console
+    fetch('http://127.0.0.1:8082/monitor_server')
+      .then(response => response.json())
+      .then(data => setServerInfo(data))
+      .catch(error => console.error('Error fetching server info:', error));
+
+    fetch('http://127.0.0.1:8082/manage_cloud')
+      .then(response => response.json())
+      .then(data => setCloudInstances(data))
+      .catch(error => console.error('Error fetching cloud instances:', error));
+
+    fetch('http://127.0.0.1:8082/send_alert')
+      .then(response => response.json())
+      .then(data => setAlertMessage(data))
+      .catch(error => console.error('Error sending alert:', error));
+
+    fetch('http://127.0.0.1:8082/secure_data_exchange')
+      .then(response => response.json())
+      .then(data => setVerificationResult(data))
+      .catch(error => console.error('Error performing secure data exchange:', error));
+  }, []);
 
   return (
     <div className="rust-modules">
@@ -30,7 +47,16 @@ const RustModules = () => {
           <li key={index}>{module}</li>
         ))}
       </ul>
-     
+      <p>Server Info:</p>
+      <pre>{serverInfo}</pre>
+      <p>Cloud Instances:</p>
+      <ul>
+        {cloudInstances.map((instance, index) => (
+          <li key={index}>{instance.id}: {instance.status}</li>
+        ))}
+      </ul>
+      <p>Alert Message: {alertMessage}</p>
+      <p>Verification Result: {verificationResult}</p>
     </div>
   );
 };
